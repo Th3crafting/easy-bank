3cabede0625ae19920d20ff5705ed331c2954c03
Dillan Real <132624193+Th3crafting@users.noreply.github.com>
2024-12-09 20:13:03 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#000',
+      white: '#fff',
+      gray: {
+        100: '#f7fafc',
+        200: '#edf2f7',
+        300: '#e2e8f0',
+        400: '#cbd5e0',
+        500: '#a0aec0',
+      }
+    },
+    extend: {},
+  },
+  variants: {
+    borderWidth: ['responsive', 'hover', 'focus'],
+  },
+  plugins: [],
+}
+
